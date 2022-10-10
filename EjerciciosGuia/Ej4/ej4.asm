3c153a710fca89d92c775ce149c6ef5460abf63e
Fabri <moboficial00@gmail.com>
2022-10-10 15:10:11 -0300

    AMOINUGS

---
+++ EjerciciosGuia/Ej4/ej4.asm
@@ -0,0 +1,17 @@
+;Template generado por compilame.sh
+global main
+; Imports de funciones de C (por defecto se importan todas, comentar con ";" para excluirlas)
+extern puts
+extern gets
+extern printf
+extern sscanf
+
+section 	.data ;Seccion con valores pre establecidos
+
+section 	.bss ;Seccion sin valor por defecto
+
+section 	.text
+main:
+
+ret
+
