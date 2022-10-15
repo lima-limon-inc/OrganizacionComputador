7948248d61d50300204116a2c91bdddb513e7267
Fabri <moboficial00@gmail.com>
2022-10-15 02:10:49 -0300

    Ejercicio 7

--- EjerciciosGuia/Ej4/ej4.asm
+++ EjerciciosGuia/Ej4/ej4.asm
@@ -11,12 +11,20 @@ extern sscanf
 
 section 	.data ;Seccion con valores pre establecidos
 	msjaUsuario db "Ingrese 15 numeros",0
-;	formatInputFillColl     db "%hi %hi %hi %hi %hi %hi %hi %hi %hi %hi %hi %hi %hi %hi %hi",0
-	formatInputFillColl     db "%hi %hi %hi %hi",0
+	msjError db "Error de conversion",0
+	msjDato db "%hi",10,0
+
+	formatInputFillColl     db "%hi",0
+
+	desplaz dq 1
+	longElemento db 64
+
 
 section 	.bss ;Seccion sin valor por defecto
-	numerosInput resw 300
+	numerosInput resb 50
 	vector 	resq 15
+	input resq 1
+	pivote resq 1
 
 section 	.text
 main:
@@ -25,22 +33,96 @@ main:
 	call puts
 	add rsp, 8
 
-	mov rdi, numerosInput ;Mando lo que me manda la gets a numerosInput
-	sub rsp, 8
-	call gets
-	add rsp, 8
+	mov rcx,15 ;Queremos iterar 15 veces
+ loopDe15:
+ 	mov r12,rcx ;Me guardo el valor del rcx en el en un registro pivote
 
-	sub rsp, 8
-	call validarInput
-	add rsp, 8
+        mov rdi, numerosInput ;Mando lo que me manda la gets a numerosInput
+        sub rsp, 8 ;15
+        call gets ; Chau
+        add rsp, 8
+
+ 	sub rsp, 8
+ 	call validarInput
+ 	add rsp, 8
+
+ 	mov rcx, r12 ;Me guardo en el rcx lo que tenia en r12 (el valor guarado antes) al rcx para que lo reste la loop
+
+ 	sub rsp, 8
+ 	call anadirAVector
+ 	add rsp, 8
+
+ 	loop loopDe15
+
+	mov rax, 1
+	mov [desplaz], rax ;Hashtag Reciclado
+
+ 	sub rsp, 8
+ 	call imprimirVector
+ 	add rsp, 8
 ret
 
 validarInput:
-	mov rdi, numerosInput 
+	mov rdi, numerosInput
 	mov rsi, formatInputFillColl
-	mov rdx, vector
+	mov rdx, input ; l 3
 	sub rsp,8
 	call sscanf
 	add rsp, 8
-	mov r10, [vector]
+	cmp rax, 1
+	jl errorDeConversion
+ret
+
+errorDeConversion:
+	mov rdi, msjError ;Le mando a la screen el mensaje de input
+	sub rsp, 8
+	call puts
+	add rsp, 8
+ret
+
+anadirAVector:
+;Input ---> el numero
+;Desplaz
+;Vector
+; (i -1) * longElemento
+; 		64
+	
+	mov r13, [desplaz]
+	sub r13, 1
+
+	sub rbx, rbx ; Como vamos a pasar un dato de menor tamanio, tenemos que limpiar el registro
+	mov bl, [longElemento] ;Aca longElemento es de 1 byte (por como lo escribimos en .data)
+
+	imul r13, rbx ;me guardo el resultado en el registro 13, y despues lo multiplicopor rbx (que tengo guardado el 64)
+
+	mov r14, [input]
+	mov [vector + r13], r14
+
+	mov rbx,[desplaz]
+	inc rbx
+	mov [desplaz], rbx
+	
+	mov r15, [vector + r13]
+; [ | | | | | | | | | | | | | |  | | | | | | | | | | | ]
+
+ret
+
+imprimirVector:
+	
+	mov r13, [desplaz]
+	sub r13, 1
+
+	sub rbx, rbx ; Como vamos a pasar un dato de menor tamanio, tenemos que limpiar el registro
+	mov bl, [longElemento] ;Aca longElemento es de 1 byte (por como lo escribimos en .data)
+
+	imul r13, rbx ;me guardo el resultado en el registro 13, y despues lo multiplicopor rbx (que tengo guardado el 64)
+
+	mov r15, [vector + r13]
+	mov [pivote], r15
+
+	mov rdi, msjDato ;Le mando a la screen el mensaje de input
+	mov rsi, [pivote]
+	sub rsp, 8
+	call printf
+	add rsp, 8
 ret
