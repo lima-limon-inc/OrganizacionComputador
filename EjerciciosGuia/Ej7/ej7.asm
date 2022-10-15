2f281cf69816a4c35faa9a9679686cefb398cfcc
Fabri <moboficial00@gmail.com>
2022-10-15 18:48:08 -0300

    Anadi a Mario

--- EjerciciosGuia/Ej7/ej7.asm
+++ EjerciciosGuia/Ej7/ej7.asm
@@ -10,13 +10,16 @@ extern fwrite
 extern fread
 extern fclose
 
-
+; Dada una matriz de 3x3 de números almacenados en BPF c/s de 16 bits, calcule la
+; traza e imprimirla por pantalla.
 
 section 	.data ;Seccion con valores pre establecidos
 
 	matriz dw 1,1,1
 	       dw 1,1,1
 	       dw 1,1,1
+	longElemento db 2 ;(3 elementos, 2 bytes cada uno)
+	longFila db 6
 
 section 	.bss ;Seccion sin valor por defecto
 
@@ -25,3 +28,7 @@ main:
 
 ret
 
+calcDesplaz:
+;(i-1) * longFila + (j-1) * longElemento
+; longFila = longElemento + cantColumnas
+ret
