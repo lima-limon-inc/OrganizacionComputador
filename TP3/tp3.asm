eb75b93fe5713c8eb465ca23ca595bfcbf3e5122
Fabri <moboficial00@gmail.com>
2022-11-13 15:22:47 -0300

    "Verifica que el archivo existe y si no, te pide uno hasta que le pases uno que existe"

--- TP3/tp3.asm
+++ TP3/tp3.asm
@@ -27,24 +27,56 @@ section 	.data ;Seccion con valores pre establecidos
 
 	;; Mensajes para la pantalla
 	msjPedirArchivo db "Bienvenido al Ordenador 3000 Ultra, necesito que me indiques que archivo queres que ordene.", 0
+	msjErrorNoExisteArchivo db "ERROR: El archivo ingresado no existe, por favor ingresar un archivo presente en el directorio actual", 0
+
+	;; Procesamiento de archivos
+	mode db "rb", 0
 
 section 	.bss ;Seccion sin valor por defecto
 
+	;; Funcionamiento del algoritmo
 	ordenarMayor resb 1 	;Aca voy a reservar 1 o 0, 1 --> Mayor; 0 ---> Menor 
-	archivoAOrdenar resd 1 	;Aca voy a guardar el nombre del archivo que el usuario quiere ordenar
+
+	;; Variables de los archivos
+	archivoAOrdenar resw 1 	;Aca voy a guardar el nombre del archivo que el usuario quiere ordenar.
+	handle resb 1		;Aca voy a guardar el handler del archivo
 
 section 	.text
 main:
-
 	sub rsp, 8
-	call bienvenida
+	call bienvenida		;En esta rutina voy a procesar el input que el usuario me diga (voy a verificar si el archivo existe). En esta rutina voy a darle la bienvenida al usuario
 	add rsp,8
 ret
 
 ;; Rutinas del main	
+
+	;; Rutina de bienvenida
+errorArchivo:
+	mov rdi, msjErrorNoExisteArchivo
+	sub rsp, 8
+	call puts
+	add rsp, 8
 bienvenida:
 	mov rdi, msjPedirArchivo
 	sub rsp, 8
 	call puts
 	add rsp, 8
+
+
+	mov rdi, archivoAOrdenar ;El usuario ingresa el archivo que quiere ordenar
+	sub rsp, 8
+	call gets
+	add rsp, 8
+	
+	mov rdi, archivoAOrdenar
+	mov rsi, mode
+	sub rsp, 8
+	call fopen
+	add rsp, 8
+
+	cmp rax, 0 		;Corroboro que el archivo exista
+	jle errorArchivo	;Si no existe, vuelvo a pedirlo
+
+	;; Si llego hasta aca, significa que el archivo existe
+	mov [handle], rax 	;En handle me guardo que paso con la apertura del archivo
 ret
