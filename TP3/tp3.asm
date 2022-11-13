1099faa13d781c09269496c52da6ce70fb1acbdc
Fabri <moboficial00@gmail.com>
2022-11-13 14:47:25 -0300

    El programa imprime a la pantalla correctamente

--- TP3/tp3.asm
+++ TP3/tp3.asm
@@ -1,18 +1,14 @@
-;Template generado por compilame.sh
-/*
-Dado un archivo que contiene n números en BPF c/signo de 8 bits (n <= 30) se pide codificar en
-assembler Intel 80x86 un programa que imprima por pantalla los movimientos que se realizan (por
-ejemplo “Realizando el intercambio de valores”) y el contenido de dicho archivo ordenado en forma
-ascendente o descendente de acuerdo a lo que elija el usuario, usando un algoritmo de ordenamiento
-basado en el método de selección.
-procedure seleccion(int[] vector)
+;; Dado un archivo que contiene n números en BPF c/signo de 8 bits (n <= 30) se pide codificar en
+;; assembler Intel 80x86 un programa que imprima por pantalla los movimientos que se realizan (por
+;; ejemplo “Realizando el intercambio de valores”) y el contenido de dicho archivo ordenado en forma
+;; ascendente o descendente de acuerdo a lo que elija el usuario, usando un algoritmo de ordenamiento
+;; basado en el método de selección.
 
-El método de ordenamiento por selección funciona seleccionando el menor elemento del vector y
-llevándolo al principio; a continuación selecciona el siguiente menor y lo pone en la segunda posición
-del vector y así sucesivamente.
-Nota: no es correcto generar el archivo con un editor de textos de forma tal que cada registro sea una tira de
-16 caracteres 1 y 0. Se aconseja el uso de un editor hexadecimal.
-*/
+;; El método de ordenamiento por selección funciona seleccionando el menor elemento del vector y
+;; llevándolo al principio; a continuación selecciona el siguiente menor y lo pone en la segunda posición
+;; del vector y así sucesivamente.
+;; Nota: no es correcto generar el archivo con un editor de textos de forma tal que cada registro sea una tira de
+;; 16 caracteres 1 y 0. Se aconseja el uso de un editor hexadecimal.
 
 global main
 ; Imports de funciones de C (por defecto se importan todas, comentar con ";" para excluirlas)
@@ -29,10 +25,26 @@ extern fclose
 
 section 	.data ;Seccion con valores pre establecidos
 
+	;; Mensajes para la pantalla
+	msjPedirArchivo db "Bienvenido al Ordenador 3000 Ultra, necesito que me indiques que archivo queres que ordene.", 0
+
 section 	.bss ;Seccion sin valor por defecto
+
+	ordenarMayor resb 1 	;Aca voy a reservar 1 o 0, 1 --> Mayor; 0 ---> Menor 
+	archivoAOrdenar resd 1 	;Aca voy a guardar el nombre del archivo que el usuario quiere ordenar
 
 section 	.text
 main:
 
+	sub rsp, 8
+	call bienvenida
+	add rsp,8
 ret
 
+;; Rutinas del main	
+bienvenida:
+	mov rdi, msjPedirArchivo
+	sub rsp, 8
+	call puts
+	add rsp, 8
+ret
